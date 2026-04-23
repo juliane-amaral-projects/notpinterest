1d91d855a3d51b51fbc37eb890a2570acdf803ba
Juliane <jucosta115@gmail.com>
2026-04-23 20:53:59 -0300

    chore: set debug to false

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fakepinterest import app
 
 # O "if" só será executado quando o arquivo "main.py" for executado
 if __name__ == "__main__":
-    app.run(debug=True) # qualquer alteração no código será implementado no site, quando este estiver no ar
+    app.run(debug=False)
+
 
 
 
