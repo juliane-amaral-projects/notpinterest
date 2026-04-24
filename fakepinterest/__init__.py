b3f4a32fbc2bcd97769f4f175a8cf47036426ca5
Juliane <jucosta115@gmail.com>
2026-04-23 22:23:52 -0300

    feat: switch database to PostgreSQL and update dependencies in requirements.txt

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask # O Flask com 'F' maiúsculo irá criar o site
 from flask_sqlalchemy import SQLAlchemy # cria o banco de dados
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 # Criação do app (aplicação), que é o site
 app = Flask(__name__)
@@ -9,7 +10,7 @@ app = Flask(__name__)
 #link
 
 # Configurar variável do APP
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config["SECRET_KEY"] = "e413aabd15f9e7cdd9afc8a234d021d6"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
